2f26723ace8946e6ebd978541ec64f0b4f1487dd
DevelopJKong <ybaram40@gmail.com>
2022-03-13 18:52:28 +0900

    Node.js Social Media REST API with MongoDb [10:01 coding]

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const PORT = 8000;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log("Connect to MongoDB");
+})
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to homepage");
+});
+
+app.listen(PORT,()=> {
+    console.log(`Backend server is running http://localhost:${PORT}`);
+});
